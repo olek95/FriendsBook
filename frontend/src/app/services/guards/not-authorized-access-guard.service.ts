fe62db63957ab6f4c0bd959bac4e69ababcf871e
olek95 <olek1995s@poczta.onet.pl>
2019-04-07 12:57:42 +0100

    Fix exiting from app when user goes to registration page from other app and he is already logged in

--- frontend/src/app/services/guards/not-authorized-access-guard.service.ts
+++ frontend/src/app/services/guards/not-authorized-access-guard.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Location } from '@angular/common';
-import { AuthorizedAccessGuard } from './authorized-access-guard.service';
-import { CanActivate, UrlTree } from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
-import {AuthorizationService} from "../authorization/authorization.service";
+import { AuthorizationService } from '../authorization/authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotAuthorizedAccessGuard implements CanActivate {
 
-  constructor(private authorizationService: AuthorizationService, private location: Location) { }
+  limitLength: number;
+
+  constructor(private authorizationService: AuthorizationService, private location: Location, private router: Router) { }
 
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const signed = this.authorizationService.isSigned();
+    if (!this.limitLength) {
+      this.limitLength = window.history.length;
+    }
     if (signed) {
-      this.location.back();
+      window.history.length > this.limitLength ? this.location.back() : this.router.navigate(['/home']);
     }
     return !signed;
   }
